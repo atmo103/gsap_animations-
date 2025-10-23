1aebd2c76e0b3089b8e68fc9a30ecb78b4d46121
atmo103 <atmo103.dev@gmail.com>
2025-10-23 16:24:37 -0500

    implementation navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className='relative min-h-screen w-screen overflow-x-hidden bg-zinc-600'>
+    <Navbar/>
      <Hero/>
-     <section className='z-0 min-h-screen bg-blue-500'/>
+
+     <About />
     </main>
   )
 }
