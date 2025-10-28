347145a1edcc23e12cd08cd37153bdb7d1a15415
atmo103 <atmo103.dev@gmail.com>
2025-10-28 09:19:23 -0500

    final implementation

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FaGithub } from 'react-icons/fa'
+
+const links = [
+    {href: 'https://github.com/atmo103', icon: <FaGithub/>},
+        {href: 'https://github.com/atmo103', icon: <FaGithub/>},
+         {href: 'https://github.com/atmo103', icon: <FaGithub/>}
+]
+
+const Footer = () => {
+  return (
+    <footer className='w-screen bg-violet-300 py-4 text-black'>
+        <div className='container mx-auto flex flex-col items-center justify-between gap-4 px-4
+        md:flex-row'>
+            <p className='text-center text-sm  md:text-left font-bold'>Atmo103 - Gsap + React UX </p>
+            <div className='flex justify-center gap-4 md:justify-start'>
+{links.map((link) => (
+    <a key={link} href={link.href} target='_blank' rel='noopener noreferrer'
+    className='text-black transition-colors duration-500 ease-in-out hover:text-white text-2xl'>{link.icon}</a>
+))}
+            </div>
+
+            
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
